529cacf6157d25fc1b204a4ad97ed4dd5833a367
Myint Myat Pyae Sone <myintmyatps664@gmail.com>
2024-05-24 23:34:17 +0630

    Added Routes Methods

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const expenseRoute = require("./routes/expense");
 
 dotenv.config();
 const app = express();
 
+//ROUTES
+app.use("/expenses", expenseRoute);
+
 // MIDDLEWARE
 app.use(cors());
 
-// DB CONNECTION
 
+// DB CONNECTION
 mongoose.connect(process.env.DB_CONNECTION).then(() => {
     console.log(`DB Connection is successful!`);
 }).catch((err) => {
--- Backend/routes/expense.js
+++ Backend/routes/expense.js
@@ -15,19 +15,42 @@ router.post("/", async (req, res) => {
 
 // GET ALL EXPENSE
 router.get("/", async (req, res) => {
-    try {
-        const expenses = Expense.find().sort({createdAt: -1});
-        res.status(200).json({expenses});
-    } catch (error) {
-        res.status(500).json({error});
-        
-    }
+  try {
+    const expenses = Expense.find().sort({ createdAt: -1 });
+    res.status(200).json({ expenses });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 // UPDATE EXPENSE
-router.put("/", (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try {
+    const expense = await Expense.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(201).sjson(expense);
+  } catch (error) {
+    res.status(500).sjson(error);
+  }
+});
 
 // DELETE AN EXPENSE
-router.delete("/", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+
+    try {
+        await Expense.findByIdAndDelete(req.params.id);
+        res.status(201).json("Deleted Successfully")
+    } catch (error) {
+        res.status(500).sjson(error);
+
+    }
+});
 
 module.exports = router;
